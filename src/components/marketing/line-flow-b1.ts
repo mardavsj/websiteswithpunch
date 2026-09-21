0fe5e718bd2db9db632c3ce0a2d48e21cd266cd0
Mardav <mardavjadaun113@gmail.com>
2026-09-21 22:21:30 +0530

    Wire line-flow-b1.ts as composer over p0/p1/p2 parts

--- src/components/marketing/line-flow-b1.ts
+++ src/components/marketing/line-flow-b1.ts
@@ -1,1 +1,4 @@
-export const LINE_FLOW_B1 = "PLACEHOLDER_B1";
+import { LINE_FLOW_B1_P0 } from "./line-flow-b1-p0";
+import { LINE_FLOW_B1_P1 } from "./line-flow-b1-p1";
+import { LINE_FLOW_B1_P2 } from "./line-flow-b1-p2";
+export const LINE_FLOW_B1 = `${LINE_FLOW_B1_P0}${LINE_FLOW_B1_P1}${LINE_FLOW_B1_P2}`;
