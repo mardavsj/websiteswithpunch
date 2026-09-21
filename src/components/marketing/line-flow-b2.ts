0756c1e903565147537aa75795fa6976c5424a6d
Mardav <mardavjadaun113@gmail.com>
2026-09-21 22:47:40 +0530

    Compose line-flow-b2 from p0+p1+p2 parts

--- src/components/marketing/line-flow-b2.ts
+++ src/components/marketing/line-flow-b2.ts
@@ -1,1 +1,4 @@
-export const LINE_FLOW_B2 = "PLACEHOLDER_B2";
+import { LINE_FLOW_B2_P0 } from "./line-flow-b2-p0";
+import { LINE_FLOW_B2_P1 } from "./line-flow-b2-p1";
+import { LINE_FLOW_B2_P2 } from "./line-flow-b2-p2";
+export const LINE_FLOW_B2 = `${LINE_FLOW_B2_P0}${LINE_FLOW_B2_P1}${LINE_FLOW_B2_P2}`;
