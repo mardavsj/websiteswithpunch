b67c602fa520d0d470ee2acd7430cd0e141f6d20
Mardav <mardavjadaun113@gmail.com>
2026-09-21 22:17:53 +0530

    Wire line-flow-a1.ts as composer over p0/p1/p2 parts

--- src/components/marketing/line-flow-a1.ts
+++ src/components/marketing/line-flow-a1.ts
@@ -1,1 +1,4 @@
-export const LINE_FLOW_A1 = "TEST";
+import { LINE_FLOW_A1_P0 } from "./line-flow-a1-p0";
+import { LINE_FLOW_A1_P1 } from "./line-flow-a1-p1";
+import { LINE_FLOW_A1_P2 } from "./line-flow-a1-p2";
+export const LINE_FLOW_A1 = `${LINE_FLOW_A1_P0}${LINE_FLOW_A1_P1}${LINE_FLOW_A1_P2}`;
